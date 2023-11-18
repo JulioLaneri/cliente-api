f2ddd22579de1d7f70332cab8e1bd0bb50983547
CesarXaio <cesararce@fiuni.edu.py.com>
2023-11-18 07:29:31 -0300

    Primeros cambios

--- src/components/LoginComponent.vue
+++ src/components/LoginComponent.vue
@@ -1,46 +1,46 @@
 <template>
-    <div>
-      <h2>Login</h2>
-      <form @submit.prevent="login">
-        <label for="email">Email:</label>
-        <input type="text" v-model="email" required />
-  
-        <label for="password">Contraseña:</label>
-        <input type="password" v-model="password" required />
-  
-        <button type="submit">Iniciar sesión</button>
-      </form>
-    </div>
-  </template>
-  
-  <script>
-  import axios from 'axios';
-  
-  export default {
-    data() {
-      return {
-        email: '',
-        password: '',
-      };
-    },
-    methods: {
-      async login() {
-        try {
-          const response = await axios.post('http://localhost:8081/auth/login', {
-            email: this.email,
-            password: this.password,
-          });
+  <div>
+    <h2>Login</h2>
+    <form @submit.prevent="login">
+      <label for="username">Usuario:</label>
+      <input type="text" v-model="username" required />
 
-          localStorage.setItem('user', JSON.stringify(response.data));
-        } catch (error) {
-          console.error('Error al iniciar sesión:', error.message);
-        }
-      },
+      <label for="password">Contraseña:</label>
+      <input type="password" v-model="password" required />
+
+      <button type="submit">Iniciar sesión</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      username: '',
+      password: '',
+    };
+  },
+  methods: {
+    async login() {
+      try {
+        const response = await axios.post('http://localhost:8085/api/auth', {
+          username: this.username,
+          password: this.password,
+        });
+
+        localStorage.setItem('user', JSON.stringify(response.data));
+        // Puedes agregar la lógica de redirección aquí si es necesario
+      } catch (error) {
+        console.error('Error al iniciar sesión:', error.message);
+      }
     },
-  };
-  </script>
-  
-  <style scoped>
-  /* Estilos específicos para este componente si es necesario */
-  </style>
-  
+  },
+};
+</script>
+
+<style scoped>
+/* Estilos específicos para este componente si es necesario */
+</style>
